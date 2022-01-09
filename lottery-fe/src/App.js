97d0082b8bf22045ea49e7f627fa14456f9c2af4
Davis Chang <davis.chang@langlive.com>
2022-01-09 17:02:38 +0800

    local contract instances

--- lottery-fe/src/App.js
+++ lottery-fe/src/App.js
@@ -1,27 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
 import web3 from './utils/web3'
+import lottery from './utils/lottery'
 
 function App() {
   console.log('version:', web3.version)
   web3.eth.getAccounts().then(console.log)
   
+  const [manager, setManager] = useState('')
+
+  useEffect(() => {
+    (async () => {
+      const manager = await lottery.methods.manager().call()
+      setManager(manager)
+    })()
+  }, [])
+
+  console.log('manager:', manager)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Lottery Contract</h2>
+        <p>This Contract is managed by {manager}</p>
     </div>
   );
 }
