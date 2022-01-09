1d12a7d10d7c0ebd8b5367cecf976d23c09a1e52
Davis Chang <davis.chang@langlive.com>
2022-01-09 16:26:35 +0800

    BREAKING CHANGE: webpack < 5 used to include polyfills for node.js core modules

--- contracts/deploy.js
+++ contracts/deploy.js
@@ -1,10 +1,10 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 const Web3 = require('web3')
-const { abi, evm } = require('../compile')
+const { abi, evm } = require('./compile')
 
 const provider = new HDWalletProvider(
   'carpet atom flag hub shine unlock resist misery short plastic goat rose',
-  'https://mainnet.infura.io/v3/9428e1df1bdd468e9c748fdf057ebbf9'
+  'https://rinkeby.infura.io/v3/9428e1df1bdd468e9c748fdf057ebbf9'
 )
 
 const web3 = new Web3(provider)
@@ -13,9 +13,10 @@ const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
   console.log('deploy account[0]:', accounts[0])
 
-  const result = await new Web3.eth.Contract(abi)
+  const result = await new web3.eth.Contract(abi)
     .deploy({ data: evm.bytecode.object })
     .send({ gas: '1000000', from: accounts[0] })
+
 
   console.log('Contract deployed to:', result.options.address)
   provider.engine.stop()
--- lottery-fe/src/App.js
+++ lottery-fe/src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import web3 from './utils/web3'
 
 function App() {
+  console.log('version:', web3.version)
+  web3.eth.getAccounts().then(console.log)
+  
   return (
     <div className="App">
       <header className="App-header">
